5e93bd621bd83c403cfa423def7e202055fe7888
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-12-23 20:01:30 +0200

    return src

---
+++ src/components/FriendList/FriendListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './friendListItem.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <li className={styles.item}>
+    <span
+      className={`${styles.status} ${
+        isOnline ? styles.online : styles.offline
+      }`}
+    ></span>
+    <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
+    <p className={styles.name}>{name}</p>
+  </li>
+);
+
+export default FriendListItem;
