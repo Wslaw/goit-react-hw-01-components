642bbb328c73b57132f66492dc4ba8594195eebc
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-12-25 04:19:20 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,71 @@
+// import React from 'react';
+// import styles from './Profile.module.css';
+// // import getRandomHexColorWhite from '../../helpers';
+
+
+// const Profile = ({ username, tag, location, avatar, stats }) => (
+//   <div className={styles.profile}>
+//     <div className={styles.description}>
+//       <img src={avatar} alt="User avatar" className={styles.avatar} />
+//       <p className={styles.name}>{username}</p>
+//       <p className={styles.tag}>@{tag}</p>
+//       <p className={styles.location}>{location}</p>
+//     </div>
+
+//     <ul className={styles.stats}>
+//       {Object.entries(stats).map(([label, quantity]) => (
+//         <li
+//           key={label}
+//           className={styles.item}
+//           // style={{ backgroundColor: getRandomHexColorWhite() }}
+//         >
+//           <span className={styles.label}>
+//             {label.charAt(0).toUpperCase() + label.slice(1)}
+//           </span>
+//           <span className={styles.quantity}>{quantity}</span>
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
+
+// export default Profile;
+
+// ***********************************************************
 import React from 'react';
 import styles from './Profile.module.css';
-// import getRandomHexColorWhite from '../../helpers';
 
+const Profile = ({ username, tag, location, avatar, stats }) => {
 
-const Profile = ({ username, tag, location, avatar, stats }) => (
-  <div className={styles.profile}>
-    <div className={styles.description}>
-      <img src={avatar} alt="User avatar" className={styles.avatar} />
-      <p className={styles.name}>{username}</p>
-      <p className={styles.tag}>@{tag}</p>
-      <p className={styles.location}>{location}</p>
+  // Преобразование объекта stats в массив пар [ключ, значение]
+  const statsArray = [];
+  for (const key in stats) {
+    if (stats.hasOwnProperty(key)) {
+      statsArray.push([key, stats[key]]);
+    }
+  }
+
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        {statsArray.map(([label, quantity]) => (
+          <li key={label} className={styles.item}>
+            <span className={styles.label}>
+              {label.charAt(0).toUpperCase() + label.slice(1)}
+            </span>
+            <span className={styles.quantity}>{quantity}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul className={styles.stats}>
-      {Object.entries(stats).map(([label, quantity]) => (
-        <li
-          key={label}
-          className={styles.item}
-          // style={{ backgroundColor: getRandomHexColorWhite() }}
-        >
-          <span className={styles.label}>
-            {label.charAt(0).toUpperCase() + label.slice(1)}
-          </span>
-          <span className={styles.quantity}>{quantity}</span>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  );
+};
 
 export default Profile;
